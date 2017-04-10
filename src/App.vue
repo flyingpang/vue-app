59efe9430cfdcdf92d61d726e0998ac66545b918
qi weng <qi weng>
2017-04-10 16:48:52 +0800

    css reset

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="he">翁棋</div>
   </div>
 </template>
 
@@ -31,30 +16,16 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './css/normalize.css';
 
-h1, h2 {
-  font-weight: normal;
+body{
+  margin:0;
+  padding:0;
+  font-size:10px;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.he{
+  width:32rem;
+  height:2rem;
+  background:#09c
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import App from './App.vue'
-// 移动端相应
-// require('./js/app_move.js')
+// 移动端相应响应
+require('./js/app_move.js')
 
 new Vue({
   el: '#app',
