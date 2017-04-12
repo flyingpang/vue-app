1fdb31eb3306e8707eda54eed36c2e9a1199c386
qi weng <qi weng>
2017-04-12 17:37:58 +0800

    shopcart

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
   <div class="he">翁棋</div>
-  <list v-bind:msse="data1"></list>
+  <list v-bind:goodsMsg="data1"></list>
   </div>
 </template>
 
 <script>
-import list from './js/component/list.vue'
+import list from './component/list.vue'
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      data1:[{title:'1',imgsrc:'./src/img/timg.jpg'},{title:'2',imgsrc:'./src/img/timg.jpg'},{title:'3',imgsrc:'./src/img/timg.jpg'},{title:'4',imgsrc:'./src/img/timg.jpg'}]
+      data1:[
+      {title:'1',imgsrc:'./src/img/timg.jpg'},
+      {title:'2',imgsrc:'./src/img/timg.jpg'},
+      {title:'3',imgsrc:'./src/img/timg.jpg'},
+      {title:'4',imgsrc:'./src/img/timg.jpg'},
+      {title:'1',imgsrc:'./src/img/timg.jpg'},
+      {title:'2',imgsrc:'./src/img/timg.jpg'},
+      {title:'3',imgsrc:'./src/img/timg.jpg'},
+      {title:'4',imgsrc:'./src/img/timg.jpg'},
+      ]
     }
   },
   components:{
---
+++ src/component/list.vue
@@ -0,0 +1,107 @@
+<template>
+  <ul>
+    <li class="list-li" v-for="(item,index,key) in goodsMsg">
+    <div class="list-li-left">
+        <img class="list-img-show" v-bind:src="item.imgsrc"  alt="" />
+    </div>
+    <div class="list-li-right">
+       <div class="list-li-title">{{item.title}}桃美人景天科、厚叶草属多肉植物，也称多浆植物，属被子植物</div>
+       <div class="list-li-descripytion">日本产的婴儿床垫，硬度较高，保护新生儿的骨骼健康发育成长，尿湿了也十分方便打理。</div>
+       <div class="list-li-price-wrap">
+          <span class="list-li-price">￥</span>
+          <span class="list-li-price-number">95.</span>
+          <span class="list-li-price">27</span>
+          <span v-on:click="shopclick(index)" class="list-li-add-shopcart"></span>
+       </div>
+    </div>
+
+
+    </li>
+  </ul>
+</template>
+
+  <script>
+  module.exports = {
+    props:['goodsMsg'],
+  data () {
+  return {
+    msg: 'Welcome to Your Vue.js App'
+  }
+},
+methods:{
+  shopclick:function(i){
+    alert(i)
+  }
+}
+}
+  </script>
+  <style lang="scss">
+    $itemBg:#e8e8e8;
+    $itemshadow:#e8e8e8;
+
+    .list-li{
+      padding:.4rem .26rem .4rem .24rem;
+      overflow:hidden;
+      border-bottom:1px solid $itemBg;
+      box-shadow:0rem 0.1rem 0.2rem $itemshadow;
+    }
+    .list-li-left{
+        //display:-webkit-flex;
+        //display:flex;
+        align-items:center;
+        float: left;
+        width: 12.5rem;
+    }
+     .list-img-show{
+      width:100%;
+      //-webkit-flex:1;
+      //flex:1;
+    }
+    .list-li-right{
+      margin-left:12.5rem;
+      padding-left: 0.5rem;
+    }
+    .list-li-title{
+      height: 1.8rem;
+      line-height: 1.8rem;
+      font-size:1.5rem;
+      margin-top: 0.4rem;
+      color: #000;
+      overflow: hidden;
+      text-overflow: clip;
+      word-break: no-break;
+    }
+    .list-li-descripytion{
+      font-size:1.1rem;
+      line-height:1.6rem;
+      height:4.8rem;
+      color:#999;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 3;
+    }
+    .list-li-price-wrap{
+      position:relative;
+      overflow:hidden;
+      color: #FC3A0E;
+      padding-top:0.5rem;
+      height:4rem;
+    }
+    .list-li-price{
+        font-size:1.2rem;
+    }
+    .list-li-price-number{
+        font-size:1.4rem;
+    }
+    .list-li-add-shopcart{
+      position:absolute;
+      width:2rem;
+      height:2rem;
+      background: url('../img/shopcart.png') center center no-repeat;
+      background-size:cover;
+      right:0.5rem;
+      bottom:0.5rem;
+    }
+  </style>
